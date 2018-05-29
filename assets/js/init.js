ec188c10331e2363c66e7e4d4d3fef894d184cb1
Jason <jasongoesplaces@gmail.com>
2018-05-29 19:41:21 -0400

    site-wide redesign

--- assets/js/init.js
+++ assets/js/init.js
@@ -4,6 +4,7 @@
     $('.sidenav').sidenav();
     $('.parallax').parallax();
     $('.modal').modal();
+    $(".dropdown-trigger").dropdown();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
